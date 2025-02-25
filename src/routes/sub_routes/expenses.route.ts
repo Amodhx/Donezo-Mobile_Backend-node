79c708c6328b364fafa00af4931c960865d7bc75
Amodhx <amxdhnanditha@gmail.com>
2025-02-25 21:16:09 +0530

    created expenses route initial routes

--- src/routes/sub_routes/expenses.route.ts
+++ src/routes/sub_routes/expenses.route.ts
@@ -1,4 +1,5 @@
 import {Router} from "express";
+import Expenses_controller from "../../controller/expenses.controller";
 
 class ExpensesRoute{
     router:Router
@@ -7,7 +8,10 @@ class ExpensesRoute{
         this.initialRoutes();
     }
     initialRoutes(){
-
+        this.router.post('/saveExpenses',Expenses_controller.saveExpenses)
+        this.router.patch('/updateExpenses',Expenses_controller.updateExpenses)
+        this.router.post('/deleteExpenses',Expenses_controller.deleteExpenses)
+        this.router.post('/getAllExpenses',Expenses_controller.getAllExpensesByUser)
     }
 }
 const ExpensesRouter = new ExpensesRoute();
