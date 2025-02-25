b32000d3b1afab57362a79ad0e0275c5e51efa38
Amodhx <amxdhnanditha@gmail.com>
2025-02-25 18:58:34 +0530

    edited model classes

--- src/model/expenses.model.ts
+++ src/model/expenses.model.ts
@@ -1,66 +1,18 @@
 class ExpensesModel{
-    private _expense_id:string
-    private _name : string
-    private _amount : string
-    private _date : string
-    private _icon : string
-    private _type : string
-
-    get expense_id(): string {
-        return this._expense_id;
-    }
-
-    set expense_id(value: string) {
-        this._expense_id = value;
-    }
-
-    get name(): string {
-        return this._name;
-    }
-
-    set name(value: string) {
-        this._name = value;
-    }
-
-    get amount(): string {
-        return this._amount;
-    }
-
-    set amount(value: string) {
-        this._amount = value;
-    }
-
-    get date(): string {
-        return this._date;
-    }
-
-    set date(value: string) {
-        this._date = value;
-    }
-
-    get icon(): string {
-        return this._icon;
-    }
-
-    set icon(value: string) {
-        this._icon = value;
-    }
-
-    get type(): string {
-        return this._type;
-    }
-
-    set type(value: string) {
-        this._type = value;
-    }
+    expense_id:string
+    name : string
+    amount : string
+    date : string
+    icon : string
+    type : string
 
     constructor(expense_id: string, name: string, amount: string, date: string, icon: string, type: string) {
-        this._expense_id = expense_id;
-        this._name = name;
-        this._amount = amount;
-        this._date = date;
-        this._icon = icon;
-        this._type = type;
+        this.expense_id = expense_id;
+        this.name = name;
+        this.amount = amount;
+        this.date = date;
+        this.icon = icon;
+        this.type = type;
     }
 }
 export default ExpensesModel;
--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -1,46 +1,14 @@
 class UserModel{
-    private _user_id:string
-    private _full_name:string
-    private _email:string
-    private _password:string
-
-    get user_id(): string {
-        return this._user_id;
-    }
-
-    set user_id(value: string) {
-        this._user_id = value;
-    }
-
-    get full_name(): string {
-        return this._full_name;
-    }
-
-    set full_name(value: string) {
-        this._full_name = value;
-    }
-
-    get email(): string {
-        return this._email;
-    }
-
-    set email(value: string) {
-        this._email = value;
-    }
-
-    get password(): string {
-        return this._password;
-    }
-
-    set password(value: string) {
-        this._password = value;
-    }
+    user_id:string
+    full_name:string
+    email:string
+    password:string
 
     constructor(user_id: string, full_name: string, email: string, password: string) {
-        this._user_id = user_id;
-        this._full_name = full_name;
-        this._email = email;
-        this._password = password;
+        this.user_id = user_id;
+        this.full_name = full_name;
+        this.email = email;
+        this.password = password;
     }
 }
 export default UserModel;
